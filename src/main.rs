2b9bfb7e6e7594134983a1c111441c2fc3b1a6a2
Amayas1 <khelfouneamayas@gmail.com>
2020-02-18 20:39:45 +0100

    fixed bug - last update

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,44 @@
-use std::str;
- 
-const UPPERCASE_OFFSET: i8 = -65;
-const LOWERCASE_OFFSET: i8 = 26 - 97;
-const NUM_OFFSET: i8 = 52 - 48;
+//KHELFOUNE Amayas khelfouneamayas@gmail.com
 
-fn base64decode(input: String) -> String {
-    let result = input.trim().chars()
-        .filter(|&ch| ch != '=')                                //Filter '=' chars
-        .map(|ch| {                                             //Map char values using Base64 Characters Table
-            let ascii = ch as i8;                           
-            let convert = match ch {
-                '0' ..= '9' => ascii + NUM_OFFSET,
-                'a' ..= 'z' => ascii + LOWERCASE_OFFSET,
-                'A' ..= 'Z' => ascii + UPPERCASE_OFFSET,
-                '+' => 62,
-                '/' => 63
+utilisez std :: str ;
+
+const UPPERCASE_OFFSET: i8  =  - 65 ;
+const LOWERCASE_OFFSET: i8  =  26  -  97 ;
+const NUM_OFFSET: i8  =  52  -  48 ;
+
+fn  base64decode (entrée: String ) -> String {
+    laissez result = input. trim (). chars ()
+        . filter ( | & ch | ch ! =  '=' )                                
+        . map ( | ch | {                                             
+            soit ascii = ch comme  i8 ;                           
+            laissez convertir =  correspondre à ch {
+                '0' .. =  '9'  => ascii + NUM_OFFSET,
+                'a' .. =  'z'  => ascii + LOWERCASE_OFFSET,
+                'A' .. =  'Z'  => ascii + UPPERCASE_OFFSET,
+                '+'  =>  62 ,
+                '/'  =>  63
             };
-            format!("{:#08b}", convert)[2..].to_string()        //convert indices to binary format and remove the two first digits
+            format! ( "{: # 08b}" , convertir) [ 2 ..]. to_string ()        
         })
-        .collect::<String>()                                    //concatenate the resulting binary values
-        .chars()
-        .collect::<Vec<char>>()
-        .chunks(8)                                              //split into 8 character chunks
-        .map(|chunk| {
-            let num_str = chunk.iter().collect::<String>();
-            usize::from_str_radix(&num_str, 2).unwrap() as u8   //convert the binary string into its u8 value
+        . collect :: < String > ()                                     
+        . chars ()
+        . collect :: < Vec < char >> ()
+        . morceaux ( 8 )                                             
+        . map ( | chunk | {
+            laissez num_str = chunk. iter (). collect :: < String > ();
+            usize :: from_str_radix ( & num_str, 2 ). unwrap () as  u8    
         })
-        .collect::<Vec<_>>();
+        . collect :: < Vec <_>> ();
  
-    let result = str::from_utf8(&result).unwrap();              //convert into UTF-8 string
-    return String::from(result);
+    laissez result =  str :: from_utf8 ( & result). dérouler ();              
+    return  String :: from (résultat);
 }
  
-fn main() {
-    let input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    println!("Input: {}", input);
+fn  main () {
+    laissez input =  String :: new ();
+    std :: io :: stdin (). read_line ( & mut entrée). dérouler ();
+    println! ( "Entrée: {}" , entrée);
  
-    let output = base64decode(input);
-    println!("Output: {}", output);
+    laissez output =  base64decode (entrée);
+    println! ( "Sortie: {}" , sortie);
 }
